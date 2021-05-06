ac670f06b7b8679050a3ce51df3cce531d4b20e9
Michael Mayaguari <mmayagu000@citymail.cuny.edu>
2021-05-05 19:13:42 -0500

    Update db.config.js

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,9 +1,9 @@
 // configure MySQL database & Sequelize
 module.exports = {
-  HOST: "sql5.freemysqlhosting.net",
-  USER: "sql5408535",
-  PASSWORD: "CNFq9khMkW",
-  DB: "sql5408535",
+  HOST: "freedb.tech",
+  USER: "freedbtech_rou",
+  PASSWORD: "1807Mich@3l",
+  DB: "freedbtech_rousp",
   dialect: "mysql",
   pool: {
     max: 5,
